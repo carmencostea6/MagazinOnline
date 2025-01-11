dfd9aed45fc6bdd9378d8a0f2058867ac73ce0db
Carmen Costea <151074871+carmencostea6@users.noreply.github.com>
2025-01-11 17:00:54 +0200

    Update Operator_Comenzi.cpp

--- tema/Operator_Comenzi.cpp
+++ tema/Operator_Comenzi.cpp
@@ -1,11 +1,33 @@
 #include "Operator_Comenzi.h"
 using namespace std;
+
+void Operator_Comenzi::adaugaComanda(shared_ptr<Comenzi> comanda)
+{
+    if (comenziActive.size() < 3)
+    {
+        comenziActive.push(comanda); // Folosește shared_ptr
+        cout << "Comanda cu ID-ul " << comanda->getID() << " a fost asignata operatorului " << getID() << endl;
+    }
+    else
+    {
+        throw std::invalid_argument("Operatorul nu poate gestiona mai mult de 3 comenzi simultan");
+    }
+}
+
+void Operator_Comenzi::adaugaComandaFinalizata(shared_ptr<Comenzi> comanda)
+{
+    comenziFinalizate.push_back(comanda);
+    nrComenziProcesate++;
+}
+
 double Operator_Comenzi:: getSalariu() const
 {
-    //NU UITA DE 0.5% DIN COMENZI
     double salariu= Salariu_Baza * coeficient + 100 * getAniVechime();
     if(esteZiuaAngajatului())
         salariu=salariu+100;
-    return salariu;
+    double bonus = 0.0;
+    // Calculez bonusul din comenzile finalizate
+    for (const auto& comanda : comenziFinalizate)
+        bonus += comanda->getValoare() * 0.005;
+    return salariu + bonus;
 }
-
